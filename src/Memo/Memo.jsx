0b436cf28cd6c7637ce8b46e67cdcc9502884945
jyc-coder <jyc4648@gmail.com>
2022-09-22 20:33:20 +0900

    #4 - draggable 컴포넌트 적용시키기
    전에 설치한 draggable을 `Memo`컴포넌트에 적용시킨다.
    
    * `return`내용을 전부 `Draggable`로 감싸고 `useRef`를 선언한뒤 드래그 시작 버튼을 `DragHandleIcon`으로 설정하기 위해 `ref`를 `props`에 추가한다.

--- src/Memo/Memo.jsx
+++ src/Memo/Memo.jsx
@@ -1,26 +1,38 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import DragHandleIcon from '@mui/icons-material/DragHandle';
 import CloseIcon from '@mui/icons-material/Close';
 import './Memo.scss';
+import Draggable from '@jyc-coder/draggable';
 const Memo = () => {
+  const handleRef = useRef(null);
   return (
-    <div
-      className="memo-container"
-      style={{ width: `${250}px`, height: `${300}px` }}
+    <Draggable
+      handleRef={handleRef}
+      x={0}
+      y={0}
+      onMove={(x, y) => console.log(x, y)}
     >
-      <div className="menu">
-        <DragHandleIcon sx={{ cursor: 'move', fontSize: '25px' }} />
-        <CloseIcon
-          sx={{ cursor: 'pointer', fontSize: '25px', float: 'right' }}
-        />
+      <div
+        className="memo-container"
+        style={{ width: `${250}px`, height: `${300}px` }}
+      >
+        <div className="menu">
+          <DragHandleIcon
+            ref={handleRef}
+            sx={{ cursor: 'move', fontSize: '25px' }}
+          />
+          <CloseIcon
+            sx={{ cursor: 'pointer', fontSize: '25px', float: 'right' }}
+          />
+        </div>
+        <textarea
+          className="memo-text-area"
+          defaultValue={'enter memo here'}
+          name="txt"
+          placeholder="Enter memo here"
+        ></textarea>
       </div>
-      <textarea
-        className="memo-text-area"
-        defaultValue={'enter memo here'}
-        name="txt"
-        placeholder="Enter memo here"
-      ></textarea>
-    </div>
+    </Draggable>
   );
 };
 
